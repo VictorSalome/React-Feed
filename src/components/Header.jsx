c9f7514cd4a5269ec112244cc4fa6ce242ef691b
Victor Salome <victorsalome41@hotmail.com>
2024-02-19 01:40:55 -0300

    Criação do projeto - Feed

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.modules.css'
+
+export const Header = () => {
+
+    return (
+        <header className='header'>
+            <strong >Ignite Feed</strong>
+        </header>
+
+    )
+}
+
