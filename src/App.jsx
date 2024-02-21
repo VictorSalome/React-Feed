1bddd530bf10b947988aba6327891d6e63c5a915
Victor Salome <victorsalome41@hotmail.com>
2024-02-21 12:46:41 -0300

    feat: Finalização da sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import { Header } from "./components/Header"
+import { Header } from "./components/Header/Header"
+import styles from './app.module.css'
+import { Post } from './post'
 
 import './global.css'
+import { Sidebar } from './components/Sidebar/Sidebar';
+
+
 
 function App() {
 
   return (
-    <>
+    <div>
       <Header />
 
-    </>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Victor Salome"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+          />
+          <Post
+            author="Gabriel Buzzi"
+            content="Um novo post muito legal"
+          />
+        </main>
+      </div>
+    </div>
   )
 }
 
