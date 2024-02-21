72258031f4dfc842eb5d270ac6d2965ac12b04d8
Victor Salome <victorsalome41@hotmail.com>
2024-02-21 15:04:48 -0300

    feat: Criação do componete post e estruturação

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { Header } from "./components/Header/Header"
-import styles from './app.module.css'
-import { Post } from './post'
+import { Header } from "./components/Header/Header";
+import styles from "./app.module.css";
+import { Post } from "./components/Post/Post";
 
-import './global.css'
-import { Sidebar } from './components/Sidebar/Sidebar';
-
-
+import "./global.css";
+import { Sidebar } from "./components/Sidebar/Sidebar";
 
 function App() {
-
   return (
     <div>
       <Header />
@@ -20,14 +17,11 @@ function App() {
             author="Victor Salome"
             content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
           />
-          <Post
-            author="Gabriel Buzzi"
-            content="Um novo post muito legal"
-          />
+          <Post author="Gabriel Buzzi" content="Um novo post muito legal" />
         </main>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
